4ada05c0029f15c91bd23fc4014410fb4897bfb2
gvgarcia <gvgarcia@users.noreply.github.com>
2016-11-25 11:59:12 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('resume', ['resume']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/resume', function (req, res) {
+  console.log('I received a GET request');
+
+  db.resume.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/resume', function (req, res) {
+  console.log(req.body);
+  db.resume.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.resume.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.resume.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.resume.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {skill: req.body.skill}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(8080);
+console.log("Server running on port 8080");
